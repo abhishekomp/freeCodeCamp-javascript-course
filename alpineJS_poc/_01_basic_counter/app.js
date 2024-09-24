918bca2aaf6f5bd56ebc4c987fc23ed974a18627
abhishekomp <abhishek2283@hotmail.com>
2024-09-24 17:40:22 +0200

    basic alpineJS data implementation

---
+++ alpineJS_poc/_01_basic_counter/app.js
@@ -0,0 +1,18 @@
+document.addEventListener('alpine:init', () => {
+  Alpine.data('counter', () => ({
+      count: 0,
+
+      init() {
+        console.log("Inside init() function")
+        this.count = 100;
+      },
+
+      increment() {
+          this.count = this.count + 1
+      },
+      
+      reset() {
+        this.count = 100
+      }  
+  }))
+})
